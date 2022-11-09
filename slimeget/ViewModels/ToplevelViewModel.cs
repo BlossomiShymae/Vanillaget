729719515a955291a42f69df093fe589731349fa
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-11-09 09:13:03 -0600

    Add view model

---
+++ slimeget/ViewModels/ToplevelViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using Terminal.Gui;
+
+namespace slimeget.ViewModels
+{
+    internal partial class ToplevelViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<MenuBarItem> _menuBarItems = new();
+
+        public ToplevelViewModel()
+        {
+            MenuBarItems = new()
+            {
+                new MenuBarItem("_File", new MenuItem[]
+                {
+                    new MenuItem("_Close", "", () =>
+                    {
+                        Application.RequestStop();
+                    })
+                    }
+                )
+            };
+        }
+    }
+}
