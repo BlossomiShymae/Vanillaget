4f132d464b31d0fa99624e561554494fc72195b1
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-11-10 12:46:23 -0600

    Add property

--- slimeget/Models/RequestMethod.cs
+++ slimeget/Models/RequestMethod.cs
@@ -3,6 +3,8 @@
 
     internal class RequestMethod
     {
+        public string Name { get; set; } = String.Empty;
+
         public string ResourcePath { get; set; } = String.Empty;
 
         public HttpMethod HttpMethod { get; set; } = HttpMethod.Get;
