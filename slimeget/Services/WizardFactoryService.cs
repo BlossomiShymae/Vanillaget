a74bfbda0a4d8ae54e005a57791b2686945a0b14
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2022-11-11 00:34:34 -0600

    Add wizard for creating request

--- slimeget/Services/WizardFactoryService.cs
+++ slimeget/Services/WizardFactoryService.cs
@@ -11,6 +11,76 @@ namespace slimeget.Services
 
         }
 
+        public Wizard CreateRequestNewWizard(Action<(TextField nameField, TextField pathField, RadioGroup methodRadioGroup)> onFinished)
+        {
+            var wizard = new Wizard
+            {
+                Title = "New Request"
+            };
+            var step = new WizardStep("Create Request Method")
+            {
+                HelpText = "Create a new request method to send."
+            };
+            wizard.AddStep(step);
+
+            // Name form
+            var nameLabel = new Label
+            {
+                Text = "Name:",
+                AutoSize = true,
+            };
+            step.Add(nameLabel);
+            var nameField = new TextField
+            {
+                X = Pos.Right(nameLabel),
+                Width = Dim.Fill() - 1,
+            };
+            step.Add(nameField);
+
+            // Resource path form
+            var pathLabel = new Label
+            {
+                Text = "Resource Path:",
+                Y = Pos.Bottom(nameLabel) + _rowMargin,
+                AutoSize = true,
+            };
+            step.Add(pathLabel);
+            var pathField = new TextField
+            {
+                X = Pos.Right(pathLabel),
+                Y = Pos.Bottom(nameField) + _rowMargin,
+                Width = Dim.Fill() - 1,
+                Height = 1,
+            };
+            step.Add(pathField);
+
+            // Http method form
+            var methodLabel = new Label
+            {
+                Text = "Http Method:",
+                Y = Pos.Bottom(pathLabel) + _rowMargin,
+                AutoSize = true,
+            };
+            step.Add(methodLabel);
+            NStack.ustring[] options = { "GET", "POST", "PUT", "DELETE", "PATCH" };
+            var methodRadioGroup = new RadioGroup(options)
+            {
+                X = Pos.Right(methodLabel),
+                Y = Pos.Bottom(pathField) + _rowMargin,
+                Width = Dim.Fill() - 1,
+                Height = 1,
+                DisplayMode = DisplayModeLayout.Horizontal
+            };
+            step.Add(methodRadioGroup);
+
+            wizard.Finished += (args) =>
+            {
+                onFinished.Invoke((nameField, pathField, methodRadioGroup));
+            };
+
+            return wizard;
+        }
+
         public Wizard CreateServerNewWizard(Action<(TextField nameField, TextField hostnameField, TextField portField)> onFinished)
         {
             var wizard = new Wizard
