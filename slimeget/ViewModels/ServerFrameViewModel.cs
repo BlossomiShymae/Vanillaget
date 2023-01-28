141cd86bbea9f54bbf0264f24d25ac96ad2b0e2b
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2023-01-28 12:29:52 -0600

    Add ability to select server

--- slimeget/ViewModels/ServerFrameViewModel.cs
+++ slimeget/ViewModels/ServerFrameViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using slimeget.Messages;
+using slimeget.Services;
+using Terminal.Gui;
 
 namespace slimeget.ViewModels
 {
@@ -8,10 +11,13 @@ namespace slimeget.ViewModels
 	[ObservableObject]
 	internal partial class ServerFrameViewModel : IRecipient<ApplicationStateMessage>
 	{
+		private ApplicationState? _applicationState;
 		[ObservableProperty]
 		private string _title = String.Empty;
 		[ObservableProperty]
-		private List<string> _serverNames = new();
+		private List<string> _servers = new();
+		[ObservableProperty]
+		private int _selectedItem = 0;
 
 		public ServerFrameViewModel(IMessenger messenger)
 		{
@@ -21,9 +27,29 @@ namespace slimeget.ViewModels
 			Messenger.Register<ApplicationStateMessage>(this);
 		}
 
+		[RelayCommand]
+		private void SelectServer(ListViewItemEventArgs e)
+		{
+			var applicationState = _applicationState;
+			if (applicationState == null) return;
+
+			var servers = applicationState.RequestMethodCollections;
+			var server = servers.ElementAtOrDefault(e.Item);
+			if (server == null) return;
+
+			applicationState.SelectedCollection = server;
+			_selectedItem = e.Item;
+			Messenger.Send<ApplicationStateMessage>(new(applicationState));
+		}
+
 		void IRecipient<ApplicationStateMessage>.Receive(ApplicationStateMessage message)
 		{
-			ServerNames = message.applicationState.RequestMethodCollections.Select(x => x.Name).ToList();
+			_applicationState = message.applicationState;
+
+			var collections = message.applicationState.RequestMethodCollections;
+			var servers = collections.Select(x => x.Name).ToList();
+			if (SelectedItem > servers.Count - 1) SelectedItem = 0;
+			Servers = servers;
 		}
 	}
 }
